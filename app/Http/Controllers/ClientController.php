e91babd5b48c2dbbca3cb5f5b2dbc4c18d49bb06
Julio Cesar <julio.silva@supero.com.br>
2016-09-27 17:22:48 -0300

    Client Json Controller

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+
+class ClientController extends Controller
+{
+    public function index(){
+        return \CodeProject\Client::all();
+    }
+}
